a3085a998be91997ffee7be560513a5da3ceb073
Morau Fiodar <fiodar.morau@sharekey.com>
2019-06-29 17:45:41 +0300

    feature: project init

---
+++ calendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calendar
+//
+//  Created by Фёдор Морев on 6/29/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ calendar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  calendar
+//
+//  Created by Фёдор Морев on 6/29/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
